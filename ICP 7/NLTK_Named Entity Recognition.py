9290e6d38cb4deb874f2e4b3d6404b33fd422fc2
Shiva Kumar Murarishetti <smpf4@mail.umkc.edu>
2020-07-01 01:07:43 -0500

    ignore

--- ICP 7/NLTK_Named Entity Recognition.py
+++ ICP 7/NLTK_Named Entity Recognition.py
@@ -1,30 +1,25 @@
-#importing nltk(Natural Language Toolkit)
+# importing nltk (Natural Language Toolkit)
 import nltk
 
-#Downloading Package punkt
+# Downloading Package punkt
 nltk.download('punkt')
-
-#opening file in read mode
+# opening file in read mode
 file = open('input.txt', 'r')
-
-#Reading the data in the file
+# Reading the data in the file
 input = file.read()
-
-
 """
 Named entity recognition is the subtask of information extraction that seeks to locate and classify elements in text into pre-defined categories such as the names of the person, organizations, locations, expressions of times, quantities, etc.
 """
 
-#Importing required packages
+# Importing required packages
 from nltk.tokenize import wordpunct_tokenize
-from nltk.tokenize import word_tokenize
 from nltk.tag import pos_tag
 from nltk import ne_chunk
 
-#Downloading required packages
+# Downloading required packages
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
 
 print("Named Entity Recognition:")
-ne_tree=ne_chunk(pos_tag(wordpunct_tokenize(input)))
+ne_tree = ne_chunk(pos_tag(wordpunct_tokenize(input)))
 print(ne_tree)
